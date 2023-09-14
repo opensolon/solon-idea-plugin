af4a719b62138fdcc1314e055cb7c5da210e0fbc
hans <837713748@qq.com>
2023-09-15 02:47:56 +0800

    1.新增对yaml配置文件代码提示的功能

--- src/main/java/org/noear/solon/idea/plugin/suggestion/completion/YamlCompletionProvider.java
+++ src/main/java/org/noear/solon/idea/plugin/suggestion/completion/YamlCompletionProvider.java
@@ -4,52 +4,51 @@ import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.lang.properties.psi.Property;
-import com.intellij.lang.properties.psi.impl.PropertyImpl;
-import com.intellij.lang.properties.psi.impl.PropertyKeyImpl;
-import com.intellij.lang.properties.psi.impl.PropertyValueImpl;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.yaml.psi.impl.YAMLPlainTextImpl;
 import org.noear.solon.idea.plugin.common.util.GenericUtil;
 import org.noear.solon.idea.plugin.suggestion.service.SuggestionService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PropertiesCompletionProvider extends CompletionProvider<CompletionParameters> {
+public class YamlCompletionProvider extends CompletionProvider<CompletionParameters> {
 
     @Override
     protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet resultSet) {
         PsiElement element = parameters.getPosition();
-        if (element instanceof PsiComment){
+        if (element instanceof PsiComment) {
             return;
         }
 
         Project project = element.getProject();
         SuggestionService suggestionService = SuggestionService.getInstance(project);
 
-        if (!suggestionService.canProvideSuggestions()){
+        if (!suggestionService.canProvideSuggestions()) {
             return;
         }
 
-        Property property = getParentOfType(element, PropertyImpl.class);
+        YAMLPlainTextImpl yaml = getParentOfType(element, YAMLPlainTextImpl.class);
+
         String queryWithDotDelimitedPrefixes = GenericUtil.truncateIdeaDummyIdentifier(element);
         List<LookupElementBuilder> elementBuilders = new ArrayList<>();
-        if (property != null && element.getClass() == PropertyKeyImpl.class){
-            elementBuilders = suggestionService.findPropertySuggestionsForQueryPrefix(queryWithDotDelimitedPrefixes);
-        }else if(property != null && element.getClass() == PropertyValueImpl.class){
-            elementBuilders = suggestionService.findHintSuggestionsForQueryPrefix(property.getKey(), queryWithDotDelimitedPrefixes);
+        if (yaml != null && queryWithDotDelimitedPrefixes.contains(": ")) {
+            elementBuilders = suggestionService.findHintSuggestionsForQueryPrefix(queryWithDotDelimitedPrefixes, queryWithDotDelimitedPrefixes);
+        } else if (yaml != null) {
+            elementBuilders = suggestionService.findYamlSuggestionsForQueryPrefix(queryWithDotDelimitedPrefixes);
+
         }
         assert elementBuilders != null;
         elementBuilders.forEach(resultSet::addElement);
     }
 
-    private <T> T getParentOfType (PsiElement element , Class<T> clazz){
+    private <T> T getParentOfType(PsiElement element, Class<T> clazz) {
         PsiElement parent = element.getParent();
-        if (parent.getClass() == clazz){
+        if (parent.getClass() == clazz) {
             return (T) parent;
         }
         return null;
---
+++ src/main/java/org/noear/solon/idea/plugin/suggestion/filetype/SolonYamlFileType.java
@@ -0,0 +1,41 @@
+package org.noear.solon.idea.plugin.suggestion.filetype;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.util.NlsSafe;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.yaml.YAMLLanguage;
+import org.noear.solon.idea.plugin.SolonIcons;
+
+import javax.swing.*;
+
+public class SolonYamlFileType extends LanguageFileType {
+
+    public static final SolonYamlFileType INSTANCE = new SolonYamlFileType();
+
+    private SolonYamlFileType() {
+        super(YAMLLanguage.INSTANCE,true);
+    }
+
+    @Override
+    public @NonNls @NotNull String getName() {
+        return "solon-yaml-file";
+    }
+
+    @Override
+    public @NlsContexts.Label @NotNull String getDescription() {
+        return "Solon yaml file";
+    }
+
+    @Override
+    public @NlsSafe @NotNull String getDefaultExtension() {
+        return "yaml";
+    }
+
+    @Override
+    public @Nullable Icon getIcon(){
+        return SolonIcons.SolonIcon_16x16;
+    }
+}
