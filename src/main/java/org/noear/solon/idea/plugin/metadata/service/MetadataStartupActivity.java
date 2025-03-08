70c5f06b5ab7e79b7c3ae54c6d467bb14a8009b7
fuzi1996 <imfuzi@163.com>
2025-03-08 13:01:24 +0800

    ft:修复耗时操作导致异常

--- src/main/java/org/noear/solon/idea/plugin/metadata/service/MetadataStartupActivity.java
+++ src/main/java/org/noear/solon/idea/plugin/metadata/service/MetadataStartupActivity.java
@@ -1,10 +1,12 @@
 package org.noear.solon.idea.plugin.metadata.service;
 
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.ProjectActivity;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.util.concurrency.AppExecutorUtil;
 import kotlin.Unit;
 import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
@@ -13,12 +15,20 @@ import org.jetbrains.annotations.Nullable;
 public class MetadataStartupActivity implements ProjectActivity {
     @Override
     public @Nullable Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
-        for (Module module : ModuleManager.getInstance(project).getModules()) {
-            ModuleMetadataService service = module.getService(ModuleMetadataService.class);
-            if (service instanceof ModuleMetadataServiceImpl impl) {
-                impl.refreshMetadata();
-            }
-        }
+        // 使用后台线程执行耗时操作
+        DumbService.getInstance(project).runWhenSmart(() -> {
+            ReadAction.nonBlocking(() -> {
+                        for (Module module : ModuleManager.getInstance(project).getModules()) {
+                            ModuleMetadataService service = module.getService(ModuleMetadataService.class);
+                            if (service instanceof ModuleMetadataServiceImpl impl) {
+                                impl.refreshMetadata();
+                            }
+                        }
+                        return null;
+                    })
+                    .inSmartMode(project)
+                    .submit(AppExecutorUtil.getAppExecutorService());
+        });
         return Unit.INSTANCE;
     }
 }
